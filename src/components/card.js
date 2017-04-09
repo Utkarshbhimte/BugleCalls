a1e1b5e5aa8933eed53d1b9be590113bc7882e93
bhimtemachine <utkarshbhimte95@gmail.com>
2017-04-09 06:36:11 +0530

    initial commit

--- src/components/card.js
+++ src/components/card.js
@@ -3,22 +3,27 @@ import React, {
     PropTypes,
 } from 'react';
 
+import FontAwesome from 'react-fontawesome';
+
+
 class Card extends Component {
     render() {
         return (
             <div className="card">
                 <span className="event-type">Hackathon</span>
-                <h4>IoT Geeks - Arduino Day 2017</h4>
-                <small className="organizer">Murugadoss Balasubramanian</small>
-                <div className="tags-wrap">
-                    <div className="tag">IoT</div>
-                </div>
-                <span className="time">Saturday, April 1 at 10 AM - 4 PM</span>
-                <span className="location">Chennai</span>
-                <div className="btn-wrap">
-                    <a href="">Link to Registration Form</a>
-                    <a href="">More Info</a>
-                    <a href="">Comments</a>
+                <div className="details">
+                    <h4>IoT Geeks - Arduino Day 2017</h4>
+                    <div className="tags-wrap">
+                        <div className="tag">IoT</div>
+                    </div>
+                    <small className="organizer">Murugadoss Balasubramanian</small>
+                    <span className="time">Saturday, April 1 at 10 AM - 4 PM</span>
+                    <span className="location">Chennai</span>
+                    <div className="btn-wrap">
+                        <a href=""><FontAwesome name='info-circle'/></a>
+                        <a href=""><FontAwesome name='file'/></a>
+                        <a href=""><FontAwesome name='commenting'/></a>
+                    </div>
                 </div>
             </div>
         );
