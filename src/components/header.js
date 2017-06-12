0110b8188b7c9a8b063f19bac249f492d5f52d4e
bhimtemachine <utkarshbhimte95@gmail.com>
2017-06-12 21:44:14 +0530

    side-nav is working

--- src/components/header.js
+++ src/components/header.js
@@ -10,6 +10,16 @@ const Header = () => {
                 <div className="line"></div>
                 <div className="line"></div>
             </label>
+            <div className="side-nav">
+                <label htmlFor="trigger" className="back-btn">
+                    Back
+                </label>
+                <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Bookmarked Events</a></li>
+                <li><a href="">About Us</a></li>
+                </ul>
+            </div>
         </header>
     )
 };
